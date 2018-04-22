8bb596774f1ba99210be94f7eee8205dfe78581d
vagrant <vagrant@master.(none)>
2018-04-23 07:49:28 +0900

    Tweet風Webアプリケーション

---
+++ tweet_like/config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+
+
+  resources :users
+  resource :registrations, only: [:new, :create]
+  resource :sessions, only: [:new, :create, :destroy]
+  resource :settings, only: [:edit, :update]
+
+  resources :users, only: [:index, :show] do
+    resource :follows, only: [:create, :destroy]
+    get :favorites, on: :member
+    get :follows, on: :member
+    get :followers, on: :member
+  end
+
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+    get :timeline, on: :collection
+  end
+
+  root to: 'registrations#new'
+  # resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
